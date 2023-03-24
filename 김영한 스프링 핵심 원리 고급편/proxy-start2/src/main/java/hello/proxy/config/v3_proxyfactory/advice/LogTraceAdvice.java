a3959bc331f39b90e501d87f5c983b759a3a50ca
jeongyun <wjddbs9179@naver.com>
2023-03-24 20:24:26 +0900

    23년 03월 24일 공부 내용 추가
    BeanPostProcessor
    스프링 빈 후처리기 이며 애플리케이션이 동작하기 전에 스프링 빈을 조작 할 수 있음. 이로써 스프링 빈으로 등록될 객체를 프록시로 바꾸는 것이 가능.
    
    @Aspect, @Around 를 사용하여 편리하게 Advisor를 적용 할 수 있음.

---
+++ 김영한 스프링 핵심 원리 고급편/proxy-start2/src/main/java/hello/proxy/config/v3_proxyfactory/advice/LogTraceAdvice.java
@@ -0,0 +1,30 @@
+package hello.proxy.config.v3_proxyfactory.advice;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import java.lang.reflect.Method;
+
+@RequiredArgsConstructor
+public class LogTraceAdvice implements MethodInterceptor {
+
+    private final LogTrace trace;
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        TraceStatus status = null;
+        try{
+            Method method = invocation.getMethod();
+            status = trace.begin(method.getDeclaringClass().getSimpleName()+"."+method.getName() + "()");
+            Object result = invocation.proceed();
+            trace.end(status);
+            return result;
+        }catch (Exception e){
+            trace.exception(status,e);
+            throw e;
+        }
+    }
+}
---
+++ 김영한 스프링 핵심 원리 고급편/proxy-start2/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -0,0 +1,38 @@
+package hello.proxy.config.v6_aop.aspect;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+@Aspect
+public class LogTraceAspect {
+
+    private final LogTrace trace;
+
+    public LogTraceAspect(LogTrace trace) {
+        this.trace = trace;
+    }
+
+    @Around("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        TraceStatus status = null;
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = trace.begin(message);
+            //로직 호출
+            Object result = joinPoint.proceed();
+
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}

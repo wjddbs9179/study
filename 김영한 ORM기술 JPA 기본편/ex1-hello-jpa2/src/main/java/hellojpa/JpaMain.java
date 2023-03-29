2edaec95edf99ef0807871639cf8ea9ef2d73d14
jeongyun <wjddbs9179@naver.com>
2023-03-29 22:01:09 +0900

    23년 03월 29일 공부내용 추가
    JPA기본편 복습
    순수 JPA를 사용 하는 경우 META-INF/persistence.xml 설정 파일 필요
    em.persist(member)  insert
    em.find(Member.class,1L) select
    member.setName("변경") find로 찾아온 member의 값을 변경하면 자동으로 update쿼리가 DB에 전달됨.
    em.remove(member) delete
    
    JPA 영속성 컨텍스트
    persist 또는 find를 호출하면 영속성 컨텍스트 1차 캐시에 객체를 저장 함.
    이후 같은 객체를 조회하면 영속성 컨텍스트에서 찾아서 반환함.
    
    JPA 쓰기지연
    JPA는 persist를 호출하면 DB에 sql을 바로 날리지 않고, 트랜잭션을 커밋하는 시점 까지 sql을 모아 뒀다가 커밋이 되는 시점에 한꺼번에 DB에 전달함.

--- 김영한 ORM기술 JPA 기본편/ex1-hello-jpa2/src/main/java/hellojpa/JpaMain.java
+++ 김영한 ORM기술 JPA 기본편/ex1-hello-jpa2/src/main/java/hellojpa/JpaMain.java
@@ -8,17 +8,17 @@ import javax.persistence.Persistence;
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
-        //code
         try {
+            Member member = em.find(Member.class, 150L);
+            member.setName("ABABD");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
-            e.printStackTrace();
         } finally {
             em.close();
         }

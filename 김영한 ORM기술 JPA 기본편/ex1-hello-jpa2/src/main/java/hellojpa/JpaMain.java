bca7069de288e5046d78c9e3aa039c96e543e4f7
jeongyun <wjddbs9179@naver.com>
2023-03-30 09:30:18 +0900

    23년 3월 30일 공부내용 추가
    jpa는 em.persist(member)를 호출한다고 해서 바로 쿼리를 DB에 전달하지 않는다.
    commit시점에 쿼리문을 전달하고 commit을 한다.
    이 로 인해서 버퍼링스 기능을 사용할 수 있다. (쿼리문을 모아뒀다가 한번에 쿼리문을 날림)
    성능면에서 각각 따로 쿼리문을 날리는거 보다 좋음.

--- 김영한 ORM기술 JPA 기본편/ex1-hello-jpa2/src/main/java/hellojpa/JpaMain.java
+++ 김영한 ORM기술 JPA 기본편/ex1-hello-jpa2/src/main/java/hellojpa/JpaMain.java
@@ -13,8 +13,13 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = em.find(Member.class, 150L);
-            member.setName("ABABD");
+
+            Member member1 = new Member(150L,"A");
+            Member member2 = new Member(160L,"B");
+
+            em.persist(member1);
+            em.persist(member2);
+            System.out.println("=========================");
 
             tx.commit();
         } catch (Exception e) {

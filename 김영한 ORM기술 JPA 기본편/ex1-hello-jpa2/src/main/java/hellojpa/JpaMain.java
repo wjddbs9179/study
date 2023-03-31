0265499ee079d9d3794d314c75260d938f9734bc
jeongyun <wjddbs9179@naver.com>
2023-03-31 13:36:14 +0900

    @MappedSuperclass
    - 공통 매핑 정보가 필요할 때 사용(id, name)
    
    - 상송관계 매핑X
    - 엔티티X, 테이블과 매핑X
    - 부모 클래스를 상속 받는 자식 클래스에 매핑 정보만 제공
    - 조회, 검색 불가(em.find(BaseEntity)불가)
    - 직접 생성해서 사용할 일이 없으므로 추상 클래스 권장
    
    - 테이블과 관계 없고, 단순히 엔티티가 공통으로 사용하는 매핑 정보를 모으는 역할
    - 주로 등록일, 수정일, 등록자, 수정자 같은 전체 엔티티에서 공통으로 적용하는 정보를 모을 때 사용
    - 참고 : @Entity 클래스는 엔티티나 @MappedSuperclass로 지정한 클래스만 상속 가능

--- 김영한 ORM기술 JPA 기본편/ex1-hello-jpa2/src/main/java/hellojpa/JpaMain.java
+++ 김영한 ORM기술 JPA 기본편/ex1-hello-jpa2/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -13,20 +14,12 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
 
-            Movie movie = new Movie();
-            movie.setPrice(10000);
-            movie.setActor("actorA");
-            movie.setName("movieA");
-            movie.setDirector("directorA");
-
-            em.persist(movie);
-
-            em.flush();
-            em.clear();
-
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie.getName());
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {

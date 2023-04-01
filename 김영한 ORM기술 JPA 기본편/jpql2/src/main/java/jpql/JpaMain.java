2701ea40ea40f3828cc3c0daaec37d79d19d64ab
jeongyun <wjddbs9179@naver.com>
2023-04-01 15:37:04 +0900

    경로 표현식
    - .(점)을 찍어 객체 그래프를 탐색하는 것
       select m.username  -> 상태 필드
          from Member m
             join m.team t      -> 단일 값 연관 필드
             join m.orders o   -> 컬렉션 값 연관 필드
        where t.name = '팀A'
    
    경로 표현식 용어 정리
    - 상태 필드(state field) : 단순히 값을 저장하기 위한 필드 (ex : m.username)
    - 연관 필드(association field) : 연관관계를 위한 필드
       - 단일 값 연관 필드 :
          @ManyToOne, @OneToOne, 대상이 엔티티(ex : m.team)
       - 컬렉션 값 연관 필드 :
          @OneToMany, @ManyToMany, 대상이 컬렉션(ex : m.orders)
    
    경로 표현식 특징
    - 상태 필드(state field) : 경로 탐색의 끝, 탐색X
    - 단일 값 연관 경로 : 묵시적 내부 조인(inner join) 발생, 탐색O
    - 컬렉션 값 연관 경로 : 묵시적 내부 조인 발생, 탐색X
       - FROM 절에서 명시적 조인을 통해 별칭을 얻으면 별칭을 통해 탐색 가능
    주의 : 묵시적 내부 조인이 발생하면 성능 튜닝이 어렵기 때문에 왠만하면 사용하면 안됨.
    
    단일 값 연관 경로 탐색
    - JPQL : select o.member from Order o
    - SQL : select m.* from Orders o inner join Member m on o.member_id = m.id
    
    명시적 조인, 묵시적 조인
    - 명시적 조인  : join 키워드 직접 사용
       - select m from Member m join m.team t
    - 묵시적 조인 : 경로 표현식에 의해 묵시적으로 SQL 조인 발생 (내부 조인만 가능)
       - select m.team from Member m
    
    경로 표현식 - 예제
    - select o.member.team from Order o -> 성공
    - select t.members from Team -> 성공
    - select t.members.username from Team t -> 실패
    - select m.username from Team t join t.members m -> 성공
    
    경로 탐색을 사용한 묵시적 조인 시 주의사항
    - 항상 내부 조인
    - 컬렉션은 경로 탐색의 끝, 명시적 조인을 통해 별칭을 얻어야 함
    - 경로 탐색은 주로 SELECT, WHERE 절에서 사용하지만 묵시적 조인으로 인해 SQL의 FROM(JOIN) 절에 영향을 줌.
    
    실무 조언
    - 가급적 묵시적 조인 대신에 명시적 조인 사용
    - 조인은 SQL 튜닝에 중요 포인트
    - 묵시적 조인은 조인이 일어나는 상황을 한눈에 파악하기 어려움

--- 김영한 ORM기술 JPA 기본편/jpql2/src/main/java/jpql/JpaMain.java
+++ 김영한 ORM기술 JPA 기본편/jpql2/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -11,26 +12,39 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            Team team1 = new Team();
+            team1.setName("teamA");
+            em.persist(team1);
+
+            Team team2 = new Team();
+            team2.setName("teamB");
+            em.persist(team2);
+
+            Team team3 = new Team();
+            team3.setName("teamC");
+            em.persist(team3);
+
 
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.changeTeam(team1);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.changeTeam(team2);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("관리자3");
+            member3.changeTeam(team3);
             em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select function('group_concat',m.username) from Member m";
-
-            List<String> result = em.createQuery(query, String.class).getResultList();
-
+            String query = "select t.members.size from Team t";
+            List<Integer> result = em.createQuery(query, Integer.class).getResultList();
             result.stream().forEach(s -> System.out.println("s = " + s));
 
             tx.commit();

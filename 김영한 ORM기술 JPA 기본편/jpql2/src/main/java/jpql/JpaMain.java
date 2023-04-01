fc88f991433601dfab222a96f24cb6da74da8039
jeongyun <wjddbs9179@naver.com>
2023-04-01 17:56:12 +0900

    JPQL 다형성 쿼리
    TYPE
    - 조회 대상을 특정 자식으로 한정
    - 예) Item 중에 Book, Movie를 조회해라
    - [JPQL]
       select i from Item i
       where type(i) IN (Book, Movie)
    - [SQL]
       select i from i
       where i.DTYPE in ('B', 'M')
    
    TREAT(JPA 2.1)
    - 자바의 타입 캐스팅과 유사
    - 상속 구조에서 부모 타입을 특정 자식 타입으로 다룰 떄 사용
    - FROM, WHERE, SELECT(하이버네이트 지원) 사용
    - 예) 부모인 Item과 자식 Book이 있다.
    - [JPQL]
       select i from Item i
       where treat(i as Book).auther = 'kim'
    - [SQL]
       select i.* from Item i
       where i.DTYPE = 'B' and i.auther = 'kim'
    
    JPQL - 엔티티 직접 사용
    
    엔티티 직접 사용 - 기본 키 값
    - JPQL에서 엔티티를 직접 사용하면 SQL에서 해당 엔티티의 기본 키 값을 사용
    - [JPQL]
       select count(m.id) from Member m // 엔티티의 아이디를 사용
       select count(m) from Member m //엔티티를 직접 사용
    - [SQL](JPQL 둘다 같은 다음 SQL 실행)
       select count(m.id) as cnt from Member m
    
    Named 쿼리 - 정적 쿼리
    - 미리 정의해서 이름을 부여해두고 사용하는 JPQL
    - 정적 쿼리
    - 어노테이션, XML에 정의
    - 애플리케이션 로딩 시점에 초기화 후 재사용
    - 애플리케이션 로딩 시점에 쿼리를 검증
    
    Named 쿼리 환경에 따른 설정
    - XML이 항상 우선권을 가진다.
    - 애플리케이션 운영 환경에 따라 다른 XML을 배포할 수 있다.
    
    JPQL 벌크 연산
    - 재고가 10개 미만인 모든 상품의 가격을 10% 상승하려면?
    - JPA 변경 감지 기능으로 실행하려면 너무 많은 SQL 실행
       - 1. 재고가 10개 미만인 상품을 리스트로 조회한다.
       - 2. 상품 엔티티의 가격을 10% 증가한다.
       - 3. 트랜잭션 커밋 시점에 변경감지가 동작한다.
    - 변경된 데이터가 100건이라면 100번의 UPDATE SQL 실행됨.
    
    벌크 연산 예제
    - 쿼리 한 번으로 여러 테이블 로우 변경(엔티티)
    - executeUpdate()의 결과는 영향받은 엔티티 수 반환
    - UPDATE, DELETE 지원
    - INSERT(insert into .. select, 하이버네이트 지원)
    
    벌크 연산 주의
    - 벌크 연산은 영속성 컨텍스트를 무시하고 데이터베이스에 직접 쿼리
       - 벌크 연산을 먼저 실행
       - 벌크 연산 수행 후 영속성 컨텍스트 초기화

--- 김영한 ORM기술 JPA 기본편/jpql2/src/main/java/jpql/JpaMain.java
+++ 김영한 ORM기술 JPA 기본편/jpql2/src/main/java/jpql/JpaMain.java
@@ -1,7 +1,9 @@
 package jpql;
 
-import javax.persistence.*;
-import java.util.Collection;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public class JpaMain {
@@ -21,34 +23,27 @@ public class JpaMain {
             em.persist(team2);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
+            member1.setUsername("회원1");
             member1.changeTeam(team1);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
+            member2.setUsername("회원2");
             member2.changeTeam(team1);
             em.persist(member2);
 
             Member member3 = new Member();
-            member3.setUsername("관리자3");
+            member3.setUsername("회원3");
             member3.changeTeam(team2);
             em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select t from Team t";
-            List<Team> result = em.createQuery(query, Team.class)
-                    .setFirstResult(0)
-                    .setMaxResults(2)
-                    .getResultList();
-            result.stream().forEach(t -> {
-                System.out.println("t.getName() = " + t.getName());
-                t.getMembers().stream().forEach(member -> {
-                    System.out.println("-  member = " + member);
-                });
-            });
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
+            System.out.println("resultCount = " + resultCount);
 
             tx.commit();
         } catch (Exception e) {

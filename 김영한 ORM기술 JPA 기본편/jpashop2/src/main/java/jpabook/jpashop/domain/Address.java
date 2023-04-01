61f979f8210f4dd5a17cadfca70ac1abf76de8ac
jeongyun <wjddbs9179@naver.com>
2023-04-01 09:47:38 +0900

    실전 예제 - 6. 값 타입 매핑

---
+++ 김영한 ORM기술 JPA 기본편/jpashop2/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,61 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class Address {
+
+    private final String city;
+    private final String street;
+    private final String zipcode;
+
+
+    public Address() {
+        city=null;
+        street=null;
+        zipcode=null;
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity())
+                && Objects.equals(getStreet(), address.getStreet())
+                && Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                '}';
+    }
+}
